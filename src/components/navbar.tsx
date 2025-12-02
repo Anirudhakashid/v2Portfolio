18527d0a6cfa6fee2e3d0a01872d5ce6d04e7758
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-12-02 14:01:21 +0530

    feat: added necessary components

---
+++ src/components/navbar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <Link
+        href="/blogs"
+        className="underline decoration-dashed underline-offset-[5px]"
+      >
+        blogs
+      </Link>
+      <Link
+        href="/projects"
+        className="underline decoration-dashed underline-offset-[5px]"
+      >
+        projects
+      </Link>
+      <Link
+        href="/experiences"
+        className="underline decoration-dashed underline-offset-[5px]"
+      >
+        experiences
+      </Link>
+    </div>
+  );
+};
---
+++ src/components/time.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function Time() {
+  const [time, setTime] = useState<string>("");
+
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+      setTime(
+        now.toLocaleString("en-GB", {
+          hour12: false,
+          hour: "2-digit",
+          minute: "2-digit",
+        })
+      );
+    };
+    updateTime();
+    const id = setInterval(updateTime, 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  return <div className="text-sm font-mono text-neutral-500">{time}</div>;
+}
