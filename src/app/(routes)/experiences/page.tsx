aa92d04a90821420fa53c67ee333a43335a09b87
Anirudha Kashid <anirudhakashid2004@gmail.com>
2025-12-04 13:08:05 +0530

    add routing pages

---
+++ src/app/(routes)/experiences/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { motion } from "framer-motion";
+
+const experiences = [
+  //   {
+  //     id: "1",
+  //     title: "Project Engineer at Wipro",
+  //     href: "https://www.wipro.com/",
+  //     date: "July 2025 - Present",
+  //   },
+  {
+    id: "2",
+    title: "project athena",
+    href: "https://x.com/AthenaF0SS",
+    date: "June 2025 - Present",
+  },
+  {
+    id: "3",
+    title: "Frontend Intern at mythyaverse",
+    href: "https://mythyaverse.com/",
+    date: "Oct - Dec 2024",
+  },
+];
+
+export default function Experience() {
+  return (
+    <section className="relative flex flex-col gap-4">
+      <div className="space-y-4">
+        {experiences.map((experience, index) => (
+          <motion.a
+            key={experience.id}
+            href={experience.href}
+            target="_blank"
+            rel="noreferrer"
+            className="relative z-10 flex cursor-pointer flex-col gap-10 rounded-lg border border-secondary-foreground bg-secondary p-4 hover:shadow-lg"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{
+              duration: 0.3,
+            }}
+          >
+            <div className="flex items-center justify-between">
+              <span className="text-xs md:text-base">{experience.date}</span>
+              <h3 className="text-xs md:text-base">
+                {experience.id === "3" ? (
+                  <>
+                    Frontend Intern at{" "}
+                    <span className="underline decoration-dashed underline-offset-[5px]">
+                      MythyaVerse
+                    </span>
+                  </>
+                ) : experience.id === "2" ? (
+                  <>
+                    Member at{" "}
+                    <span className="underline decoration-dashed underline-offset-[5px]">
+                      Project Athena
+                    </span>
+                  </>
+                ) : (
+                  experience.title
+                )}
+              </h3>
+            </div>
+          </motion.a>
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ src/app/(routes)/projects/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+const projects = [
+  {
+    id: "freehandX",
+    title: "freehandX",
+    description:
+      "a real-time collaborative whiteboard app for brainstorming and ideation.",
+    href: "https://freehandx.gauravsingh.co.in/",
+  },
+  {
+    id: "domcraft",
+    title: "domcraft",
+    description:
+      "a chrome extension that lets you modify website using natural language.",
+    href: "https://drive.google.com/file/d/1OMP5khnNDDO1lxSSXiGf7Oih-UyNP1FV/view?usp=drive_link",
+  },
+  {
+    id: "qrtool",
+    title: "qr-tool",
+    description: "a tool for generating qr.",
+    href: "https://tool.qr.gauravsingh.co.in/",
+  },
+  {
+    id: "pdf",
+    title: "chat-with-pdf",
+    description: "a web app that allows you to chat with your PDF documents.",
+    href: "https://chat-with-pdf-zeta-six.vercel.app/",
+  },
+];
+
+export default function Projects() {
+  return (
+    <motion.div className="relative flex flex-col gap-4">
+      {projects.map((project) => (
+        <motion.a
+          key={project.id}
+          href={project.href}
+          target="_blank"
+          className="relative z-10 -mx-1 flex cursor-pointer flex-col gap-1 rounded-lg border border-secondary-foreground bg-secondary p-4  hover:bg-secondary/80 hover:shadow-lg"
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.98 }}
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.3 }}
+          rel="noreferrer"
+        >
+          <h1 className="relative z-10 text-sm font-medium md:text-base">
+            {project.title}
+          </h1>
+          <p className="text-sm text-secondary-foreground relative z-10">
+            {project.description}
+          </p>
+        </motion.a>
+      ))}
+    </motion.div>
+  );
+}
